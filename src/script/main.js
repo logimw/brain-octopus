09218c97a806d89032cbf5c7050701fc3c72e6b3
Mateusz-Wójcik <mateusz-wojcik@hotmail.com>
2021-12-20 00:53:21 +0100

    added another slider; changed font

--- src/script/main.js
+++ src/script/main.js
@@ -1,8 +1,28 @@
-var swiper = new Swiper(".swiper", {
+new Swiper("#slider1 #coursesSlider", {
     slidesPerView: 4,
     clickable: true,
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '#slider1 .swiper-button-next',
+        prevEl: '#slider1 .swiper-button-prev',
+    },
+});
+new Swiper("#slider2 #authorsSlider", {
+    slidesPerView: 1,
+    clickable: true,
+    navigation: {
+        nextEl: '#slider2 .swiper-button-next',
+        prevEl: '#slider2 .swiper-button-prev',
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+new Swiper("#slider3 #medicalCoursesSlider", {
+    slidesPerView: 4,
+    clickable: true,
+    navigation: {
+        nextEl: '#slider3 .swiper-button-next',
+        prevEl: '#slider3 .swiper-button-prev',
     },
 });
